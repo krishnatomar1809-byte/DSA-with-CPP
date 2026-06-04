56f0db239d89572e7927fa62fbcfbb3fed33ca73
Krishna Tomar <krishnatomar1809@gmail.com>
2026-06-04 11:26:41 +0530

    Added Count Bit

---
+++ Bit-Manipulation/Count-bits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int countbit(int num){
+    int count=0;
+    while(num>0){
+        int lastdig= num & 1;
+        count += lastdig;
+        num= num>>1;
+    }
+    return count;
+}
+int main(){
+    cout<<countbit(13);
+    return 0;
+}
