5b993b2b6b506a91850d386c03df9e0640ca522c
Krishna Tomar <krishnatomar1809@gmail.com>
2026-06-22 14:42:33 +0530

    Added factorial with  Recursion

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int func(int n){
+    if(n==0){
+        return 1;
+    }
+
+    return n * func(n-1);
+}
+
+int main(){
+    cout<<func(5);
+    return 0;
+}
